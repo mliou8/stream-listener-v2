75b2961fd326119c1e6e261a05518bef1b754bfc
mliou8 <chunghanliou@gmail.com>
2016-08-21 20:30:48 -0400

    adding a react npm library and testing it out

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import YouTube from 'react-youtube';
+
+const videoIdA = 'XxVg_s8xAms';
+const videoIdB = '-DX3vJiqxm4';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>You're in my world now!!! </h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      videoId: videoIdA,
+      player: null,
+    };
+    this.onReady = this.onReady.bind(this);
+    this.onChangeVideo = this.onChangeVideo.bind(this);
+    this.onPlayVideo = this.onPlayVideo.bind(this);
+    this.onPauseVideo = this.onPauseVideo.bind(this);
+  }
+
+onReady(event) {
+  console.log(`YouTube Player object for videoId: "${this.state.videoId}" has been saved to state.`); // eslint-disable-line
+  this.setState({
+    player: event.target,
+  });
+}
+
+onPlayVideo() {
+  this.state.player.playVideo();
+}
+
+onPauseVideo() {
+  this.state.player.pauseVideo();
+}
+
+onChangeVideo() {
+  this.setState({
+    videoId: this.state.videoId === videoIdA ? videoIdB : videoIdA,
+  });
+}
+
+render() {
+  return (
+      <div>
+        <YouTube videoId={this.state.videoId} onReady={this.onReady} />
+        <button onClick={this.onPlayVideo}>Play</button>
+        <button onClick={this.onPauseVideo}>Pause</button>
+        <button onClick={this.onChangeVideo}>Change Video</button>
       </div>
     );
-  }
+    }
 }
 
 export default App;

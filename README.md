dc37a1bea005437254c63a0e02d2f5e99c291bfd
mliou8 <chunghanliou@gmail.com>
2016-08-28 19:48:38 -0400

    started adding in react-fire components

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+Todos:
+
+- Currently need to 
+
 Below you will find some information on how to perform common tasks.  
 You can find the most recent version of this guide [here](https://github.com/facebookincubator/create-react-app/blob/master/template/README.md).
 
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../App.css';
 import YouTube from 'react-youtube';
 
 const videoIdA = 'XxVg_s8xAms';
 const videoIdB = '-DX3vJiqxm4';
 
-class App extends Component {
+var config = {
+  apiKey: "AIzaSyBexPxMhJxoKGW6CNaYJKeWY1Krgd9Efmw",
+  authDomain: "test-3c2a1.firebaseapp.com",
+  databaseURL: "https://test-3c2a1.firebaseio.com",
+  storageBucket: "",
+};
+firebase.initializeApp(config);
+
+var converter = new Showdown.converter();
+
+class Player extends Component {
+  
   constructor(props) {
     super(props);
     
@@ -14,6 +25,7 @@ class App extends Component {
       videoId: videoIdA,
       player: null,
     };
+    
     this.onReady = this.onReady.bind(this);
     this.onChangeVideo = this.onChangeVideo.bind(this);
     this.onPlayVideo = this.onPlayVideo.bind(this);
@@ -27,11 +39,18 @@ onReady(event) {
   });
 }
 
+onPlaying(event) {
+  console.log("This is an event", event);
+}
+
 onPlayVideo() {
+  console.log("I guess this is playing now");
   this.state.player.playVideo();
 }
 
 onPauseVideo() {
+  console.log("I guess this is pausing now");
+  console.log(this.state.player.PlayerState);
   this.state.player.pauseVideo();
 }
 
@@ -41,10 +60,12 @@ onChangeVideo() {
   });
 }
 
+
 render() {
   return (
       <div>
-        <YouTube videoId={this.state.videoId} onReady={this.onReady} />
+        <img src={logo} className="App-logo" alt="logo" />
+        <YouTube videoId={this.state.videoId} onReady={this.onReady} onPlay={this.onPlaying} />
         <button onClick={this.onPlayVideo}>Play</button>
         <button onClick={this.onPauseVideo}>Pause</button>
         <button onClick={this.onChangeVideo}>Change Video</button>
@@ -53,4 +74,4 @@ render() {
     }
 }
 
-export default App;
+export default Player;
